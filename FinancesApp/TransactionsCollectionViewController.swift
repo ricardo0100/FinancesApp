73a62d1d0c4231a4da14b3e1bc7ff820da707b74
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2015-10-29 09:05:11 -0200

    TransactionsCollectionViewController

---
+++ FinancesApp/TransactionsCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TransactionsCollectionViewController.swift
+//  FinancesApp
+//
+//  Created by Ricardo Gehrke Filho on 29/10/15.
+//  Copyright © 2015 Ricardo Gehrke Filho. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class TransactionsCollectionViewController: UICollectionViewController {
+    
+}
